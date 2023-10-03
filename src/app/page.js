89f6b1c95596d8b32b1c4f65417052d4b832d6fe
Umar Raza <umaarrr0786@gmail.com>
2023-10-03 17:47:33 +0500

    working on about us

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Globalreach from "./_components/globalreach";
+import Aboutus from "./_components/aboutus";
 export default function Home() {
   return (
     <main className={styles.main}>
       <Globalreach />
+      <Aboutus />
     </main>
   );
 }
