016b19efa4c89c8a4be39417185b8c2a15dec615
Umar Raza <umaarrr0786@gmail.com>
2023-10-04 02:07:48 +0500

    Created Accordian Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import Globalreach from "./_components/globalreach";
 import Aboutus from "./_components/aboutus";
+import FAQ from "./_components/faq";
 export default function Home() {
   return (
     <main className={styles.main}>
       <Globalreach />
       <Aboutus />
+      <FAQ />
     </main>
   );
 }
