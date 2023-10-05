8a6805315684b71156a9550ec9c204592634d88d
Areeb <areebmhsn@gmail.com>
2023-10-05 09:12:47 +0500

    Footer Complete

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import NavBar from "./_Components/NavBar";
 import WelcomePage from "./_Components/WelcomePage";
 import Bar from "./_Components/Bar";
 import WhyPage from "./_Components/WhyPage";
+import Footer from "./_Components/Footer";
 const page = () => {
   return (
     <>
@@ -11,6 +12,7 @@ const page = () => {
     <WelcomePage/>
     <Bar />
     <WhyPage/>
+    <Footer/>
     </>
   );
 };
